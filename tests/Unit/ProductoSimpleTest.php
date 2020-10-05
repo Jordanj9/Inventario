a77bc3a59b216d8a9349e602d71b62965af296a9
Jordanj9 <jordan_j9@hotmail.com>
2020-10-05 16:14:02 -0500

    Cuarto caso de prueba v1.0

--- tests/Unit/ProductoSimpleTest.php
+++ tests/Unit/ProductoSimpleTest.php
@@ -65,4 +65,22 @@ class ProductoSimpleTest extends TestCase
         $result = $productoSimple->salida(0);
         $this->assertEquals('La cantidad es incorrecta', $result);
     }
+
+    /**
+     * Escenario:  Salida correcta de productos simples
+     * HU 1. COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS
+     * Criterio de Aceptación:
+     * 1. La cantidad de la de debe ser mayor a 0.
+     * 2. En caso de un producto sencillo la cantidad de la salida se le disminuirá a la cantidad existente del producto.
+     * Dado
+     * El usurario tiene un producto con el nombre “gaseosa litro”, costo “2000”, precio “5000” cantidad “4”
+     * Cuando    va a registrar la salida con una cantidad de 2
+     * Entonces    El sistema registrará la salida AND presentará el mensaje. “El nuevo stock del producto gaseosa litro es 2”.
+     * @test
+     */
+    public function testSalidaCorrectaProductoSimple(): void {
+        $productoSimple = new ProductoSimple('gaseosa litro', 2000, 5000, 4, 'NO');
+        $result = $productoSimple->salida(2);
+        $this->assertEquals('El nuevo stock del producto gaseosa litro es 2', $result);
+    }
 }

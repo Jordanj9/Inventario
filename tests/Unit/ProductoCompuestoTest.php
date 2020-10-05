ff94b2faf256836d6b8eb43e74f2d048f3184fde
Jordanj9 <jordan_j9@hotmail.com>
2020-10-05 16:50:28 -0500

    Quinto caso de prueba v1.0

---
+++ tests/Unit/ProductoCompuestoTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Src\Inventario\Domain\ProductoSimple;
+
+class ProductoCompuestoTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample() {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Escenario: Salida correcta de productos compuestos
+     * HU 1. COMO USUARIO QUIERO REGISTRAR LA SALIDA PRODUCTOS
+     * Criterio de Aceptación:
+     * 1   La cantidad de la de debe ser mayor a 0.
+     * 3   En caso de un producto compuesto la cantidad de la salida se le disminuirá a la cantidad existente de cada uno de su ingrediente
+     * Dado
+     * El usurario tiene los siguientes productos
+     * un producto con el nombre “salchicha”, costo “1000”, cantidad “4”
+     * un producto con el nombre “pan perro”, costo “1000”, cantidad “3”
+     * un producto con el nombre “lamina de queso”, costo “1000”, cantidad “5”
+     * El usurario tiene un producto compuesto con el nombre “Perro sencillo”, costo “3000”=>calculado, precio “5000”
+     * Cuando    va a registrar la salida con la cantidad “1”
+     * Entonces    El sistema registrará la salida AND presentará el mensaje. “El Nuevo stock de los productos: salchicha es 3, pan perro es 2, lamina de queso es 4 ”.
+     * @test
+     */
+    public function testSalidaCorrectaProductoCompuesto(): void {
+        $ingredientes = [];
+        $ingredientes[] = new ProductoSimple('salchica', 1000, null, 4, 'SI');
+        $ingredientes[] = new ProductoSimple('pan perro', 1000, null, 3, 'SI');
+        $ingredientes[] = new ProductoSimple('lamina queso', 1000, null, 5, 'SI');
+        $productoCompuesto = new ProductoCompuest($ingredientes, 5000);
+        $result = $productoCompuesto->salida();
+        self::assertEquals('El Nuevo stock de los productos: salchicha es 3, pan perro es 2, lamina de queso es 4', $result);
+    }
+}
